419057aebe0158f940aacc7e45a714d17290bf71
Diego Bahamondez <diego.bahamondezm@gmail.com>
2024-05-22 13:49:15 +0200

    fixed url for class based view

--- big_picture_library/books/urls.py
+++ big_picture_library/books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('isbn/<str:isbn>/', views.fetch_book_details, name='fetch_book_details'),
+    path('isbn/<str:isbn>/', views.FetchBookDetailsView.as_view(), name='fetch_book_details'),
     path('books/', views.BookView.as_view(), name='book_view')
 ]
