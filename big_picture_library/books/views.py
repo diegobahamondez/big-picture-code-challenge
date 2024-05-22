c4fa0f4dd2b8979775329de6669738a1368599a9
Diego Bahamondez <diego.bahamondezm@gmail.com>
2024-05-22 13:51:19 +0200

    unused import

--- big_picture_library/books/views.py
+++ big_picture_library/books/views.py
@@ -9,7 +9,6 @@ from typing import Union
 
 from django.http import JsonResponse
 from django.views import View
-from django.views.decorators.http import require_http_methods
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
